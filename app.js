d92de585bb4bcb1dee096fa43fd5f6b14665717c
laibarajpoot <128062421+laibarajpoot@users.noreply.github.com>
2023-05-17 18:42:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,183 @@
+// Chapter 1 (Alerts)
+// Question 1
+alert("Laiba");
+alert("Habib");
+alert("laibarajput216@gmail.com");
+alert(043217890);
+alert("m nhi btaonga");
+
+// Question 2
+alert("Alert!You are learning javascript!");
+// Question 3
+alert("I dont like to give any message");
+
+
+// Chapter 2
+// Variables for strings:
+
+// Q1
+var fullName
+// Q2
+var lastName
+lastName = "Laiba";
+// Q3 
+var teamName = 'Doremon';
+alert(teamName);
+// Q4
+var bestMan = "Imran";
+bestMan = "Khan";
+
+// Chapter 3
+// Variables for Numbers
+
+// Q1
+var caseQty;
+// Q2
+caseQty = 144;
+// Q3
+var num = 9;
+// Q4
+var sum = 2 + 2;
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+// Q6
+var num1 = 12222;
+var num1 = num1 + 2345;
+console.log(num1);
+
+// Chapter 4
+// Vriable names (Legal and illegal)
+
+// Q1
+var productCost = 3.45;
+// Q2
+var nameOfBand;
+// Q3
+var $total_Cost1 = 123;
+// Q4
+var fullName = firstName + lastName;
+// Q5
+// Legal variables
+var fullName
+var $full
+var full2
+var full_name
+// Illegal variables
+var full name
+var 1full
+var full-name
+var alert
+
+// Chapter 5
+// Math Expression I
+// Q1
+// Modulo operator %
+// Q2
+var num = 20 % 6;
+console.log(num);
+// Q3
+var largeNum = 1000 * 2000;
+// Q4
+var num1 = 3
+var num2 = 6
+var subtract = num1 - num2;
+// Q5
+var remainder = 20 % 6;
+console.log(remainder);
+// Q6
+alert(4*8);
+
+// Chapter 6
+// Math Expression II
+// Q1
+x++
+// Q2
+x = 100
+x--
+console.log(x);
+// Q3 
+// 50
+var x = 50;
+var y = x++
+console.log(y);
+// Q4
+// 49
+var y = 50;
+var z = --y;
+console.log(z);
+// Q5
+var t = 2
+var newNum = t--;
+console.log(t);
+console.log(newNum);
+// Q6
+var u = 3;
+var originalNum = u++
+console.log(u);
+console.log(originalNum);
+// Q7
+var n = 12;
+alert(n++);
+
+// Chapter 7 (Math Expression III)
+// Q1
+var calculatedNum = 2 + (2 * 6);
+// 14
+// Q2
+var calculatedNum = (2 + 2)*6
+// 24
+// Q3
+var calculatedNum = (2 + 2) * (4 + 2);
+// 24 
+// Q4
+var calculatedNum = ((2 + 2) * 4) + 2;
+// 18
+
+// Q5 (producing 56)
+var cost = (2 + 2) * (4 + 10);   
+// Q6 (producing 20)
+var units = 2 + (2 * 4) + 10; 
+// Q6 (producing 5)
+var pressure = 4/2*4;
+
+
+// Chapter 8 (Concatenating text strings)
+// Q1
+// "22"
+// Q2
+var message = "Hello," + "Dolly";
+alert(message);
+
+// Q3
+alert("33" + 3);
+// 333
+// Q4
+alert("Pakistan" + " " + "Zindabad");
+// Q5
+var textNumber = "string" + 12345;
+console.log(textNumber);
+// Q6
+var fname = "Kulsoom";
+var lname = "Kazmi";
+var fullName = fname + " " + lname;
+console.log(fullName);
+
+
+// Chapter 9 (Prompts)
+// Q1
+var firstName = prompt("Enter first name");
+// Q2
+var country = prompt("Country?", "China");
+// Q3
+var yourName = prompt("Enter Your Name");
+// Q4
+var message = prompt("Welcome to the website");
+// Q5
+var msg = prompt("Hello");
+var defaultResponse = prompt("Default response");
+var userResponse = prompt("Enter your response");
+ 
+
